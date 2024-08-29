c9a9af339e778e03df7f2ba9a308ea19d2ee812b
20221080030160 <bn6382012@gmail.com>
2024-08-29 11:58:24 -0300

    novo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,15 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pagamentosPU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
         Pagamento pg = new Pagamento();
         pg.setId(11);
         pg.setValor(500);
         pg.setData(new Date());
         System.out.println(pg );
+        entityManager.persist(pg);
+        entityManager.getTransaction().commit();
+        entityManagerFactory.close();
+        entityManager.close();
     }
 }
