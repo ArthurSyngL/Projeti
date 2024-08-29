b578c7988ee9ffc4e4bce552265a7bf5a3b6d25d
20221080030160 <bn6382012@gmail.com>
2024-08-29 11:53:08 -0300

    novo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import model.Pagamento;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pagamentosPU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         Pagamento pg = new Pagamento();
         pg.setId(11);
         pg.setValor(500);
